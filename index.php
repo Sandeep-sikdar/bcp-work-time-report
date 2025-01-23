5f39a5f8556c25e44237b589c4ed34f541558500
bcp-sandeep <sandeep.sikdar@bimcap.com>
2025-01-23 15:43:11 +0530

    min chng

--- index.php
+++ index.php
@@ -509,7 +509,7 @@
     $settings = json_decode(file_get_contents(__DIR__ . '/settings.json'), true);
     ?>
     <h1 style="margin:0;">Work Time Report</h1>
-    <!-- <?php echo htmlspecialchars($settings['access_token']); ?> -->
+    <?php echo htmlspecialchars($settings['access_token']); ?>
 
     <form id="myForm" method="POST">
         <div class="commonFields">
